8c573dc2695667eb86a2c3f707d06e090bcb28ec
LEE WEI YING <148225902+emery97@users.noreply.github.com>
2024-04-15 14:20:41 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class Question2
 {
   public static void main(String[] args)
-  {
+  {    
     /**
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
      * Hint: in.nextDouble() to read double
      */
-     
     Scanner in = new Scanner(System.in);
+
+    // Prompt user to enter height
+    System.out.print("Enter your height in meters: ");
     double height = in.nextDouble();
+
+    // Prompt user to enter weight
+    System.out.print("Enter your weight in kilograms: ");
+    double weight = in.nextDouble();
+
+    // Calculate BMI
+    double BMI = weight / (height * height);
+
+    // Print BMI
+    System.out.println("Your BMI is: " + BMI);
   }
 }
